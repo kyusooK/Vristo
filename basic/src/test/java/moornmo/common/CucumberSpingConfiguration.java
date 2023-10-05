371ed113e605e0357abc89d77031a97affd2cce1
kyusoo <rbtn110@uengine.org>
2023-10-05 10:48:48 +0900

    menu add

--- basic/src/test/java/moornmo/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package moornmo.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import moornmo.BasicApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { BasicApplication.class })
-public class CucumberSpingConfiguration {}
